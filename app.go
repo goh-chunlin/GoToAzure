537405c7321ca934e0b9f514a3b4d96d9a41e048
Goh Chun Lin <chunlin.goh@haulio.io>
2019-09-01 20:02:26 +0800

    Init with codes and static files.

---
+++ app.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("static/img"))))
+	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("static/fonts"))))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/api", api)
+	http.ListenAndServe(getPort(), nil)
+}
+
+func getPort() string {
+	p := os.Getenv("HTTP_PLATFORM_PORT")
+	if p != "" {
+		return ":" + p
+	}
+	return ":80"
+}
+
+func render(response http.ResponseWriter, templateFileName string) {
+
+	templateFileName = fmt.Sprintf("static/views/%s", templateFileName)
+	template, err := template.ParseFiles(templateFileName)
+
+	if err != nil {
+		log.Print("template parsing error: ", err)
+	}
+
+	err = template.Execute(response, nil)
+
+	if err != nil {
+		log.Print("template executing error: ", err)
+	}
+}
+
+func home(response http.ResponseWriter, request *http.Request) {
+	render(response, "index.html")
+}
+
+func api(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	apiStatus := modelAPIStatus{
+		Status:  true,
+		Message: "The API method is called successfully.",
+	}
+	output, _ := json.MarshalIndent(&apiStatus, "", "\t")
+
+	response.WriteHeader(200)
+	response.Write(output)
+}
---
+++ modelApiStatus.go
@@ -0,0 +1,6 @@
+package main
+
+type modelAPIStatus struct {
+	Status  bool   `json:"status"`
+	Message string `json:"message"`
+}
